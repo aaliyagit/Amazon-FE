4b17c8fdbf9b02bf5105131eb58e0677446beb7f
aaliyagit <149709011+aaliyagit@users.noreply.github.com>
2023-12-12 13:46:11 -0500

    Update Main.tf

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "Jenkins-sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-0fc5d935ebf8bc3bc"
   instance_type          = "t2.large"
-  key_name               = "Mumbai"
+  key_name               = "key11"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
